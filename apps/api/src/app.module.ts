5187eae1aa336a739084dcc92e9e5810cedd585f
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-04-11 17:13:47 -0300

    remove web package for now

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProblemsModule } from './problems/problems.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [ProblemsModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
---
+++ apps/api/src/problems/problems.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ProblemsController } from './problems.controller';
+import { ProblemsService } from './problems.service';
+
+@Module({
+	providers: [ProblemsService],
+	controllers: [ProblemsController],
+})
+export class ProblemsModule {}
