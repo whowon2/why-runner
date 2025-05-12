19a0ad79f664a4f6833187e7b3727b24b7734fbc
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-05-12 12:59:27 -0300

    fix: update validateEnvs

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AuthModule } from "./auth/auth.module";
+import { validateEnvs } from "./env";
 import { ProblemsModule } from "./problems/problems.module";
 import { SubmissionsModule } from "./submissions/submissions.module";
 import { UserModule } from "./user/user.module";
@@ -10,7 +11,7 @@ import { UserModule } from "./user/user.module";
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			validate,
+			validate: validateEnvs,
 		}),
 		ProblemsModule,
 		SubmissionsModule,
