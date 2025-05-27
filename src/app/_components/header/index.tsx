a0222fa1d38ee9ccac10092eefee3796a5bc262c
whowon2 <86119976+whowon2@users.noreply.github.com>
2025-05-27 14:33:18 -0300

    chore(header): link to login

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -1,13 +1,20 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { auth } from "@/server/auth";
+import Link from "next/link";
 import { AvatarButton } from "./avatar-button";
 
 export async function Header() {
-	const session = await auth();
+  const session = await auth();
 
-	return (
-		<div className="sticky flex items-center justify-between p-4">
-			{session ? <AvatarButton session={session} /> : <div>login</div>}
-		</div>
-	);
+  return (
+    <div className="sticky flex items-center justify-between p-4">
+      {session ? (
+        <AvatarButton session={session} />
+      ) : (
+        <Link href={"/api/auth/signin"}>
+          <Button>Login</Button>
+        </Link>
+      )}
+    </div>
+  );
 }
