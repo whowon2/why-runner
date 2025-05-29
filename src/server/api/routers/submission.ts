7dabdf4d676678a238b9d9be0c5074db60a10b42
whowon2 <86119976+whowon2@users.noreply.github.com>
2025-05-29 16:19:37 -0300

    remove bullmq

--- src/server/api/routers/submission.ts
+++ src/server/api/routers/submission.ts
@@ -1,66 +1,53 @@
-import { Queue } from "bullmq";
+import { env } from "@/env";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import Redis from "ioredis";
 import { z } from "zod";
 
-import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-
-import { env } from "@/env";
-import Redis from "ioredis";
-
 const createSubmissionInput = z.object({
-	code: z.string(),
-	language: z.enum(["c", "cpp", "java", "python", "rust"]),
-	problemId: z.string().uuid(),
+  code: z.string(),
+  language: z.enum(["c", "cpp", "java", "python", "rust"]),
+  problemId: z.string().uuid(),
 });
 
-const connection = new Redis(env.REDIS_URL);
+const redis = new Redis(env.REDIS_URL);
 
-const queue = new Queue("submissions", {
-	connection,
-	defaultJobOptions: {
-		backoff: {
-			type: "exponential",
-			delay: 1000,
-		},
-	},
-});
+async function addSubmissionJob(data: { submissionId: string }) {
+  await redis.lpush("submissions", JSON.stringify(data));
+}
 
 export const submissionRouter = createTRPCRouter({
-	create: protectedProcedure
-		.input(createSubmissionInput)
-		.mutation(async ({ ctx, input }) => {
-			const submission = await ctx.db.submission.create({ data: input });
+  create: protectedProcedure
+    .input(createSubmissionInput)
+    .mutation(async ({ ctx, input }) => {
+      const submission = await ctx.db.submission.create({ data: input });
 
-			const item = await queue.add("processSubmission", {
-				submissionId: submission.id,
-			});
+      await addSubmissionJob({ submissionId: submission.id });
 
-			console.log("queue item", item);
+      return submission;
+    }),
 
-			return submission;
-		}),
+  find: protectedProcedure
+    .input(
+      z.object({
+        problemId: z.string(),
+      }),
+    )
+    .query(({ ctx, input }) => {
+      return ctx.db.submission.findMany({
+        where: {
+          problemId: input.problemId,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }),
 
-	find: protectedProcedure
-		.input(
-			z.object({
-				problemId: z.string(),
-			}),
-		)
-		.query(({ ctx, input }) => {
-			return ctx.db.submission.findMany({
-				where: {
-					problemId: input.problemId,
-				},
-				orderBy: {
-					createdAt: "desc",
-				},
-			});
-		}),
-
-	findOne: protectedProcedure.input(z.string()).query(({ ctx, input }) => {
-		return ctx.db.submission.findUnique({
-			where: {
-				id: input,
-			},
-		});
-	}),
+  findOne: protectedProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.db.submission.findUnique({
+      where: {
+        id: input,
+      },
+    });
+  }),
 });
