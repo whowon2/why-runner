4f1ba40a5bab12adf789ea3b886b074c0435ff6f
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-12-03 22:48:58 -0300

    remove old loader

--- app/[locale]/contests/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "lucide-react";
-
-export default async function Loading() {
-  return (
-    <div className="flex w-full flex-col items-center justify-center gap-4 p-4">
-      <Loader className="animate-spin" />
-    </div>
-  );
-}
