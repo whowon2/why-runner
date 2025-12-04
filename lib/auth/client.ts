69522d08a5cef667eef293be83f2857f064842d1
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-12-03 22:44:43 -0300

    fix: authclient prod url

--- lib/auth/client.ts
+++ lib/auth/client.ts
@@ -4,6 +4,6 @@ import type { auth } from ".";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [inferAdditionalFields<typeof auth>()],
 });
