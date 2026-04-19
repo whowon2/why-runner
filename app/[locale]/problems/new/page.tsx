f8c8ff56363eaaf74f8b011791733e1afbf8d77c
whowon2 <86119976+whowon2@users.noreply.github.com>
2026-04-18 23:13:03 -0300

    refactor: remove unused user object from NewProblem component in problem creation page

--- app/[locale]/problems/new/page.tsx
+++ app/[locale]/problems/new/page.tsx
@@ -2,11 +2,11 @@ import { getCurrentUser } from "@/lib/auth/get-current-user";
 import { NewProblem } from "../_components/create";
 
 export default async function Page() {
-  const user = await getCurrentUser({ redirectTo: "/auth/signin" });
+  await getCurrentUser({ redirectTo: "/auth/signin" });
 
   return (
     <div>
-      <NewProblem user={user} />
+      <NewProblem />
     </div>
   );
 }
